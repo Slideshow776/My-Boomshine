41aa5f828f64a917c8542bef65d94f58726f3dd1
Sandra Moen <sandramoen01@gmail.com>
2024-05-22 08:27:02 +0200

    minor GDScript style guide refactoring

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,25 +1,45 @@
+class_name Game
 extends Node2D
 
-@onready var background = $background
 const BALL = preload("res://scenes/ball.tscn")
+const NUM_BALLS: int = 5
 
-func _ready():
+@onready var background = $background
+@onready var camera_2d = $Camera2D
+
+
+func _ready(): # override
 	background.modulate = Color.STEEL_BLUE
 	
-	for i in range(5):
-		spawn_ball()
+	for i in range(NUM_BALLS):
+		_spawn_ball()
 
-func _input(event):
-	if event.is_action_pressed("my_quit") and GlobalConfig.IS_DEBUG:
+func _input(event):	 # override
+	#if event.is_action_pressed("my_action"):
+		#spawn_explosion()
+	if (
+			event.is_action_pressed("my_quit") and
+			GlobalConfig.IS_DEBUG
+	):
 		get_tree().quit()
 
-func spawn_ball():
+#func spawn_explosion():
+	#var explosion = EXPLOSION.instantiate()
+	#explosion.position = get_global_mouse_position()
+	#add_child(explosion)
+
+
+func _spawn_ball(position: Vector2 = Vector2()):
 	var ball = BALL.instantiate()
-	ball.position = get_random_position()	
+	ball.position = _get_random_position()	
+	add_to_group("balls", true)
 	add_child(ball)
 
-func get_random_position() -> Vector2:
+func _get_random_position() -> Vector2:	
+	var camera_width = camera_2d.get_zoom() * get_viewport_rect().size.x
+	var camera_height = camera_2d.get_zoom() * get_viewport_rect().size.y
+	
 	return Vector2(
-		randi() % int(get_viewport().size.x),
-		randi() % int(get_viewport().size.y)
+		randf_range(-camera_width.x / 2, camera_width.x / 2),
+		randf_range(-camera_width.y / 4, camera_width.y / 4)
 	)
--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,40 +1,37 @@
+class_name Ball
 extends CharacterBody2D
 
+const MAX_VELOCITY: int = 200
+const MIN_VELOCITY: int = -200
+
 @export var bounciness: float = 1.0
+
 @onready var sprite_2d = $Sprite2D
 @onready var collision_shape_2d = $CollisionShape2D
 
-const MIN_VELOCITY = 50
-const MAX_VELOCITY = 200
 
-func _ready() -> void:
-	velocity = get_random_velocity()
-	sprite_2d.modulate = get_random_dark_color()
+func _ready(): # override
+	velocity = _get_random_velocity()
+	sprite_2d.modulate = _get_random_dark_color()
 
-func _physics_process(delta: float) -> void:
-	# Move the ball and handle collisions
-	var remaining_velocity = move_and_slide()
+func _physics_process(delta: float): # override
+	move_and_slide()
+	_handle_wall_bounce()
 
-	# Check for collisions and handle bouncing
-	var collision_count = get_slide_collision_count()
-	for i in range(collision_count):
+
+func explode():
+	queue_free()
+	
+	
+func _handle_wall_bounce():
+	for i in range(get_slide_collision_count()):
 		var collision = get_slide_collision(i)
-		if collision:
-			# Calculate the reflection vector
-			var normal = collision.get_normal()
+		if get_slide_collision(i):			
+			var normal = collision.get_normal() # Calculate the reflection vector
 			velocity = velocity.bounce(normal) * bounciness
 			break  # React only to the first collision in this example
 
-
-func _input_event(viewport, event, shape_idx):
-	if event.is_action_pressed("my_action"):
-		handle_mouse_click()
-
-func handle_mouse_click() -> void:
-	print("Ball clicked! ID:", get_instance_id())
-	# Add your custom logic here
-
-func get_random_dark_color() -> Color:
+func _get_random_dark_color() -> Color:
 	var min_value = randf_range(0, .1)  # Minimum value for color component
 	var max_value = 0.5  # Maximum value for color component
 	var channel = randi() % 3  # Randomly select a color channel: 0 (red), 1 (green), or 2 (blue)
@@ -55,8 +52,8 @@ func get_random_dark_color() -> Color:
 
 	return Color(r, g, b)
 
-func get_random_velocity() -> Vector2:
+func _get_random_velocity() -> Vector2:
 	return Vector2(
 		randi_range(MIN_VELOCITY, MAX_VELOCITY),
 		randi_range(MIN_VELOCITY, MAX_VELOCITY)
-)
+	)
